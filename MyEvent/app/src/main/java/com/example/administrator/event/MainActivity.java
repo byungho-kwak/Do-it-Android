99d5935a16f56f37352c9d129e65f426cf472ae9
kwak <byunghokwak5497@gmail.com>
2017-11-30 22:30:17 +0900

    1) keyEvent  처리 2) View selector xml 옵션 (xml을 통한 배경 이미지 지정)

--- MyEvent/app/src/main/java/com/example/administrator/event/MainActivity.java
+++ MyEvent/app/src/main/java/com/example/administrator/event/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.administrator.event;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
         TextView textView;
         ScrollView scrollView;
+        GestureDetector gestureDetector;
 
-        @Override
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -40,7 +44,69 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        gestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                println("onDown 호출됨");
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+                println("onShowPress 호출됨");
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                println("onSingleTapUp 호출됨");
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                println("onScroll 호출됨 : " + distanceX + ", " + distanceY);
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                println("onLongPress 호출됨");
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                println("onFling 호출됨" + velocityX + ", " + velocityY);
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                return true;
+            }
+        });
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            Toast.makeText(this, "back 키를 눌렀습니다.", Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     private void println(String data) {
         textView.append(data + "\n");
